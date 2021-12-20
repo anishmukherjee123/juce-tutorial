c2d1d9bc80ca364ef0c41004b417acbadb2d7e53
Anish Mukherjee <anish@Anishs-MacBook-Pro.local>
2021-12-20 14:59:37 -0500

    Added MIDI volume slider to graphics

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -16,6 +16,16 @@ FirstProjectAudioProcessorEditor::FirstProjectAudioProcessorEditor (FirstProject
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (200, 200);
+    
+    // set attributes of the slider
+    midiVolume.setSliderStyle (juce::Slider::LinearBarVertical); // style of slider (vertical)
+    midiVolume.setRange (0.0, 127.0, 1.0); // value range
+    midiVolume.setTextBoxStyle (juce::Slider::NoTextBox, false, 90, 0); // no label
+    midiVolume.setPopupDisplayEnabled (true, false, this); //
+    midiVolume.setTextValueSuffix (" Volume");
+    midiVolume.setValue(1.0);
+    // make the slide visible
+    addAndMakeVisible(&midiVolume);
 }
 
 FirstProjectAudioProcessorEditor::~FirstProjectAudioProcessorEditor()
@@ -26,15 +36,18 @@ FirstProjectAudioProcessorEditor::~FirstProjectAudioProcessorEditor()
 void FirstProjectAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll(juce::Colours::white);
 
-    g.setColour (juce::Colours::white);
+    g.setColour (juce::Colours::black);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("MIDI Volume", 0, 0, getWidth(), 30, juce::Justification::centred, 1);
 }
 
 void FirstProjectAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    
+    // position of slider
+    midiVolume.setBounds(40, 30, 20, getHeight() - 60);
 }
